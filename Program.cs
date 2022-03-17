b5627bccde78abbeea3b46d59593d47940097254
TheWolffOfWallStreet <90858305+TheWolffOfWallStreet@users.noreply.github.com>
2022-03-17 12:50:43 -0400

    Changed input responses and trying to implement a yes/no regex that allows the user to play or quit the application

--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
-﻿namespace GuessTheNumber
-{
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace GuessTheNumber
+{ 
     class program
     {
         static void Main(string[] args)
         {
-            Random r = new Random();
-            int winNum = r.Next(0, 50);
+            Random n = new Random();
+            int winNum = n.Next(0, 50);
             bool win = false;
-            Console.WriteLine("Guess a number between zero and fifty");
+            
+            Console.WriteLine("Do you want to play a game, Georgie?");
+            Console.WriteLine("Yes or no? (Y/N");
+           // do
+          //  {
+              //  string input = Console.ReadLine();
+               // string pattern = ;
+              //  Regex r = new Regex(@"^(Y|N){1}$", RegexOptions.IgnoreCase);
+              //  if (r.IsMatch(input)) ;
+                //input = Console.ReadLine();
+              //  if (r.Match() == true) ;
+                Console.WriteLine("Guess a number between zero and fifty");
+            //} while (true);
+            
             do
             {
                
@@ -22,19 +38,18 @@
                         Console.WriteLine("This is not a number");
                     }
                 } while (!parseSucceeded);
-                
 
                 if (i > winNum)
                 {
-                    Console.WriteLine("The number you guessed is TOO DAMN HIGH! Guess again!");
+                    Console.WriteLine("Sorry Georgie, your guess was too high! Guess again!");
                 }
                 else if (i < winNum)
                 {
-                    Console.WriteLine("The number you guessed is TOO DAMN LOW! Guess again!");
+                    Console.WriteLine("Sorry Georgie, your guess was too low! Guess again!");
                 }
                 else if (i == winNum)
                 {
-                    Console.WriteLine("Yer a wizard, Harry!");
+                    Console.WriteLine("Great job, Georgie! Come join me in the sewer!");
                     win = true;
                 }
             } while (win == false);
